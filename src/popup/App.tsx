49671b38184710ba5581d2eae52a7612d4b715af
consciousnesswave <consciousnesswave@gmail.com>
2025-05-10 23:22:47 +0800

    feat: Adds control panel UI with mode selection

--- src/popup/App.tsx
+++ src/popup/App.tsx
@@ -1,8 +1,32 @@
+import { SettingFilled } from "@ant-design/icons";
+import { Flex, Radio, Typography } from "antd";
+import type { CheckboxGroupProps } from "antd/es/checkbox";
+
+const { Text } = Typography;
+
 const App: React.FC = () => {
+  const options: CheckboxGroupProps<string>['options'] = [
+    { label: 'Direct', value: '0' },
+    { label: 'System', value: '1' },
+    { label: 'Proxy', value: '2' },
+    { label: 'Auto', value: '3' }
+  ];
+
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <>
+      <Flex justify="space-between">
+        <Text strong>Control Panel</Text>
+        <SettingFilled />
+      </Flex>
+      <Radio.Group
+        style={{ marginTop: 10 }}
+        block
+        options={options}
+        defaultValue="1"
+        optionType="button"
+        buttonStyle="solid"
+      />
+    </>
   );
 }
 
